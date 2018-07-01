f289cd095c48a6a6d819ce5439a77f27b4d25383
Sergi Granell <xerpi.g.12@gmail.com>
2018-07-01 11:55:49 +0200

    Simplify test/start.s

--- test/start.s
+++ test/start.s
@@ -1,24 +1,14 @@
 	.text
 	.global _start
 _start:
-	csrr x1, cycle
-	csrr x2, cycle
-	csrr x3, cycle
-	csrr x4, cycle
-	csrr x5, cycle
-
 	li x1, 0xAA00
 	li x2, 0xBB
 	add x3, x2, x1
 	sub x4, x3, x2
 
 	la x5, var
-	lw x6, 0(x5)
 	sw x4, 0(x5)
 	lw x6, 0(x5)
-
-	nop
-	nop
 
 	.data
 var:
